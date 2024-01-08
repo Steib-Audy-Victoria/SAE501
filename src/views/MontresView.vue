5bc762a048c8e2f739e386a255cbad52c61b7e4c
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2024-01-08 16:09:39 +0100

    init + lien avec api (affichage montres)

---
+++ src/views/MontresView.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>Liste des Montres</h1>
+
+  <li v-for="montre in montres" :key="montre.ID">
+    {{ montre.NomMontre }} - {{ montre.PrixMontre }}
+  </li>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      montres: []
+    }
+  },
+  mounted() {
+    axios
+      .get('http://localhost:4000/montres')
+      .then((response) => {
+        this.montres = response.data
+      })
+      .catch((error) => {
+        console.error('Erreur lors de la récupération des montres', error)
+      })
+  }
+}
+</script>
