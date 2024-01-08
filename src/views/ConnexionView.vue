18f41445f532820b5b9c2f4389bab614af2527eb
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2024-01-08 16:10:56 +0100

    connexion utilisateurs

---
+++ src/views/ConnexionView.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <h1>Inscription</h1>
+    <form @submit.prevent="inscrire">
+      <label>Nom d'utilisateur:</label>
+      <input v-model="inscriptionNom" required />
+
+      <label>Mot de passe:</label>
+      <input type="password" v-model="inscriptionMotDePasse" required />
+
+      <button type="submit">S'inscrire</button>
+    </form>
+
+    <h1>Connexion</h1>
+    <form @submit.prevent="connecter">
+      <label>Nom d'utilisateur:</label>
+      <input v-model="connexionNom" required />
+
+      <label>Mot de passe:</label>
+      <input type="password" v-model="connexionMotDePasse" required />
+
+      <button type="submit">Se connecter</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      inscriptionNom: '',
+      inscriptionMotDePasse: '',
+      connexionNom: '',
+      connexionMotDePasse: ''
+    }
+  },
+  methods: {
+    inscrire() {
+      axios
+        .post('http://localhost:4000/inscription', {
+          Nom: this.inscriptionNom,
+          MotDePasse: this.inscriptionMotDePasse
+        })
+        .then((response) => {
+          console.log('Inscription réussie, ID utilisateur:', response.data.userID)
+        })
+        .catch((error) => {
+          console.error("Erreur lors de l'inscription", error)
+        })
+    },
+    connecter() {
+      axios
+        .post('http://localhost:4000/connexion', {
+          Nom: this.connexionNom,
+          MotDePasse: this.connexionMotDePasse
+        })
+        .then((response) => {
+          console.log('Connexion réussie, ID utilisateur:', response.data.userID)
+        })
+        .catch((error) => {
+          console.error('Erreur lors de la connexion', error)
+        })
+    }
+  }
+}
+</script>
+
+<style></style>
