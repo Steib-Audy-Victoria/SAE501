49b228d366ab0ddeb6d9490e81239ad25f475f0c
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2024-01-09 15:57:26 +0100

    modification d'une montre dans la base de donnée

--- src/views/MontreView.vue
+++ src/views/MontreView.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Détails de la Montre</h1>
 
-    {{ montre }}
+    <br />
 
     <div v-if="montre">
       <p><strong>ID Montre:</strong> {{ montre.MontreID }}</p>
@@ -13,7 +13,84 @@
       <p><strong>Bracelet:</strong> {{ montre.NomBracelet }}</p>
       <p><strong>Texture Bracelet:</strong> {{ montre.TextureBracelet }}</p>
       <p><strong>Prix Total:</strong> {{ montre.PrixTotal }} €</p>
+
+      <br />
+
+      <!-- Formulaire de modification -->
+
+      <form @submit.prevent="updateWatch">
+        <!-- Champ pour le nom de la montre-->
+        <label for="NomMontre">Nouveau Nom de la Montre :</label>
+        <input v-model="newNomMontre" type="text" required />
+
+        <br />
+
+        <!-- Champ pour le boitier-->
+        <label for="BoitierID">Nouveau Boitier :</label>
+        <select v-model="newBoitierID" required>
+          <option v-for="boitier in boitiers" :key="boitier.BoitierID" :value="boitier.BoitierID">
+            {{ boitier.NomBoitier }}
+          </option>
+        </select>
+
+        <br />
+
+        <!-- Champ pour la texture du boitier-->
+        <label for="TextureBoitierID">Nouvelle Texture du Boîtier :</label>
+        <select v-model="newTextureBoitierID" required>
+          <option
+            v-for="textureBoitier in texturesBoitier"
+            :key="textureBoitier.TextureBoitierID"
+            :value="textureBoitier.TextureBoitierID"
+          >
+            {{ textureBoitier.NomTexture }}
+          </option>
+        </select>
+
+        <br />
+
+        <!-- Champ pour les pierre -->
+        <label for="PierreID">Pierre :</label>
+        <select v-model="newPierreID" required>
+          <option v-for="pierre in pierres" :key="pierre.PierreID" :value="pierre.PierreID">
+            {{ pierre.NomPierre }}
+          </option>
+        </select>
+
+        <br />
+
+        <!-- Champ pour le bracelet -->
+        <label for="BraceletID">Bracelet :</label>
+        <select v-model="newBraceletID" required>
+          <option
+            v-for="bracelet in bracelets"
+            :key="bracelet.BraceletID"
+            :value="bracelet.BraceletID"
+          >
+            {{ bracelet.NomBracelet }}
+          </option>
+        </select>
+
+        <br />
+
+        <!-- Champ pour la texture du bracelet -->
+        <label for="TextureBraceletID">Texture du Bracelet :</label>
+        <select v-model="newTextureBraceletID" required>
+          <option
+            v-for="textureBracelet in texturesBracelet"
+            :key="textureBracelet.TextureBraceletID"
+            :value="textureBracelet.TextureBraceletID"
+          >
+            {{ textureBracelet.NomTexture }}
+          </option>
+        </select>
+
+        <br />
+
+        <button type="submit">Enregistrer les modifications</button>
+      </form>
     </div>
+
     <div v-else>
       <p>Aucune montre trouvée avec cet ID.</p>
     </div>
@@ -26,13 +103,25 @@ import axios from 'axios'
 export default {
   data() {
     return {
-      montre: null
+      montre: null,
+      newNomMontre: '',
+      newBoitierID: null,
+      newTextureBoitierID: null,
+      newPierreID: null,
+      newBraceletID: null,
+      newTextureBraceletID: null,
+      boitiers: [],
+      texturesBoitier: [],
+      pierres: [],
+      bracelets: [],
+      texturesBracelet: []
     }
   },
   mounted() {
     const MontreId = this.$route.params.id
     console.log(MontreId)
 
+    // Requête GET pour récupérer les détails de la montre
     axios
       .get(`http://localhost:4000/montre/${MontreId}`)
       .then((response) => {
@@ -45,6 +134,80 @@ export default {
       .catch((error) => {
         console.error('Erreur lors de la récupération des détails de la montre', error)
       })
+
+    // Récupérer la liste des boîtiers
+    axios
+      .get('http://localhost:4000/boitiers')
+      .then((response) => {
+        this.boitiers = response.data
+      })
+      .catch((error) => {
+        console.error('Erreur lors de la récupération des boîtiers', error)
+      })
+
+    // Récupérer la liste des textures de boîtiers
+    axios
+      .get('http://localhost:4000/texturesBoitier')
+      .then((response) => {
+        this.texturesBoitier = response.data
+      })
+      .catch((error) => {
+        console.error('Erreur lors de la récupération des textures de boîtiers', error)
+      })
+
+    // Récupérer la liste des pierres
+    axios
+      .get('http://localhost:4000/pierres')
+      .then((response) => {
+        this.pierres = response.data
+      })
+      .catch((error) => {
+        console.error('Erreur lors de la récupération des pierres', error)
+      })
+
+    // Récupérer la liste des bracelets
+    axios
+      .get('http://localhost:4000/bracelets')
+      .then((response) => {
+        this.bracelets = response.data
+      })
+      .catch((error) => {
+        console.error('Erreur lors de la récupération des bracelets', error)
+      })
+
+    // Récupérer la liste des textures de bracelets
+    axios
+      .get('http://localhost:4000/texturesBracelet')
+      .then((response) => {
+        this.texturesBracelet = response.data
+      })
+      .catch((error) => {
+        console.error('Erreur lors de la récupération des textures de bracelets', error)
+      })
+  },
+  methods: {
+    // Fonction appelée lors de la soumission du formulaire de mise à jour
+    updateWatch() {
+      // Récupérer l'ID de la montre à partir des paramètres de l'URL
+      const MontreId = this.$route.params.id
+
+      // Utiliser une requête PUT pour mettre à jour les détails de la montre
+      axios
+        .put(`http://localhost:4000/montre/${MontreId}/modif`, {
+          NomMontre: this.newNomMontre,
+          BoitierID: this.newBoitierID,
+          TextureBoitierID: this.newTextureBoitierID,
+          PierreID: this.newPierreID,
+          BraceletID: this.newBraceletID,
+          TextureBraceletID: this.newTextureBraceletID
+        })
+        .then((response) => {
+          console.log(response.data.message)
+        })
+        .catch((error) => {
+          console.error('Erreur lors de la mise à jour de la montre', error)
+        })
+    }
   }
 }
 </script>
