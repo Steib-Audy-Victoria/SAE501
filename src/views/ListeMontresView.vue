c454e3ea52808435311ab1227826de5177384a7c
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2024-01-10 12:26:38 +0100

    liste des montres configurée par l'utilisateur

--- src/views/ListeMontresView.vue
+++ src/views/ListeMontresView.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
     <h1>Liste des Montres Configurées</h1>
-    <ul>
-      <li v-for="montre in montres" :key="montre.MontreID">
-        {{ montre }}
-      </li>
-    </ul>
+    <li v-for="montre in montres" :key="montre.MontreID">
+      <p><strong>ID Montre:</strong> {{ montre.MontreID }}</p>
+      <p><strong>Nom Montre:</strong> {{ montre.NomMontre }}</p>
+      <p><strong>Nom Boitier:</strong> {{ montre.NomBoitier }}</p>
+      <p><strong>Nom Texture du Boitier:</strong> {{ montre.TextureBoitier }}</p>
+      <p><strong>Nom Pierre:</strong> {{ montre.NomPierre }}</p>
+      <p><strong>Nom Bracelet:</strong> {{ montre.NomBracelet }}</p>
+      <p><strong>Nom Texture du Bracelet:</strong> {{ montre.TextureBracelet }}</p>
+      <p><strong>Prix Total:</strong> {{ montre.PrixTotal }} €</p>
+      <RouterLink :to="`/montre/${montre.MontreID}`">Voir la montre</RouterLink>
+      <hr />
+    </li>
   </div>
 </template>
 
