0f3fe8ec26e741908b6833c072a500102fbee4a2
Steib-Audy-Victoria <95017413+Steib-Audy-Victoria@users.noreply.github.com>
2024-01-10 14:23:34 +0100

    suppression d'un article du panier de l'utilisateur

--- src/views/PanierUtilisateurView.vue
+++ src/views/PanierUtilisateurView.vue
@@ -9,6 +9,10 @@
         <p><strong>Texture Boitier:</strong> {{ article.TextureBoitier }}</p>
         <p><strong>Quantité:</strong> {{ article.Quantite }}</p>
         <p><strong>Prix Total:</strong> {{ article.PrixTotal }} €</p>
+
+        <br />
+        <!-- Ajout du bouton "Supprimer du Panier" -->
+        <button @click="supprimerDuPanier(article.PanierID)">Supprimer du Panier</button>
         <hr />
       </li>
     </ul>
@@ -24,6 +28,59 @@ export default {
   data() {
     return {
       panier: []
+    }
+  },
+  methods: {
+    // Méthode pour supprimer un article du panier
+    supprimerDuPanier(PanierID) {
+      const token = localStorage.getItem('token')
+
+      if (!token) {
+        alert('Vous devez être connecté pour supprimer une montre du panier.')
+        this.$router.push('/connexion')
+        return
+      }
+
+      const headers = { Authorization: token }
+
+      // Envoyer la requête pour supprimer l'article du panier
+      axios
+        .delete(`http://localhost:4000/panier/${PanierID}/suppr`, { headers })
+        .then((response) => {
+          console.log(response.data.message)
+          alert('Article supprimé du panier avec succès!')
+          // Actualiser la liste du panier après la suppression
+          this.actualiserPanier()
+        })
+        .catch((error) => {
+          console.error(
+            "Erreur lors de la suppression de l'article du panier",
+            error.response.data.error
+          )
+          alert("Erreur lors de la suppression de l'article du panier. Veuillez réessayer.")
+        })
+    },
+
+    // Actualiser la liste du panier
+    actualiserPanier() {
+      const token = localStorage.getItem('token')
+      if (!token) {
+        alert('Vous devez être connecté pour actualiser le panier.')
+        this.$router.push('/connexion')
+        return
+      }
+      const headers = { Authorization: token }
+
+      // Envoyer la requête pour récupérer la liste mise à jour du panier
+      axios
+        .get('http://localhost:4000/panier/liste', { headers })
+        .then((response) => {
+          this.panier = response.data
+        })
+        .catch((error) => {
+          console.error('Erreur lors de la récupération du panier', error)
+          alert('Erreur lors de la récupération du panier. Veuillez réessayer.')
+        })
     }
   },
   mounted() {
